b4ce41652b5acc57a9c488379048c6b6fb746b54
mwangi-njuguna <mwanginjuguna59@gmail.com>
2018-04-24 18:45:12 +0300

    Added setup method Defined setup method according to docopt and pypi specifications

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ from thermos import __version__
 this_dir = abspath(dirname(__file__))
 with open(join(this_dir, 'README.rst'), encoding='utf-8') as file:
     long_description = file.read()
+
 class RunTests(Command):
     """Run all tests."""
     description = 'run tests'
@@ -28,3 +29,41 @@ class RunTests(Command):
         """Run all tests!"""
         errno = call(['py.test', '--cov=thermos', '--cov-report=term-missing'])
         raise SystemExit(errno)
+
+setup(
+    name = 'thermos',
+    version = __version__,
+    description = 'A command line program in Python for creating flask standard application structure, blueprints and templates.',
+    long_description = long_description,
+    url = 'https://github.com/mwangi-njuguna/thermos-cli',
+    author = 'Mwangi Njuguna',
+    author_email = 'mwanginjuguna59@gmail.com',
+    license = 'UNLICENSE',
+    classifiers = [
+        'Intended Audience :: Flask Developers',
+        'Topic :: Utilities',
+        'License :: Public Domain',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords = ['cli','flask'],
+    packages = find_packages(exclude=['docs', 'tests*']),
+    install_requires = ['docopt'],
+    extras_require = {
+        'test': ['coverage', 'pytest', 'pytest-cov'],
+    },
+    entry_points = {
+        'console_scripts': [
+            'thermos=thermos.cli:main',
+        ],
+    },
+    cmdclass = {'test': RunTests},
+)
