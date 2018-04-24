eaa0a51e066cf2ceced5a8330486d19431ae27bc
mwangi-njuguna <mwanginjuguna59@gmail.com>
2018-04-24 18:37:36 +0300

    Added class to run tests  in the setup

--- setup.py
+++ setup.py
@@ -13,3 +13,18 @@ from thermos import __version__
 this_dir = abspath(dirname(__file__))
 with open(join(this_dir, 'README.rst'), encoding='utf-8') as file:
     long_description = file.read()
+class RunTests(Command):
+    """Run all tests."""
+    description = 'run tests'
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        """Run all tests!"""
+        errno = call(['py.test', '--cov=thermos', '--cov-report=term-missing'])
+        raise SystemExit(errno)
