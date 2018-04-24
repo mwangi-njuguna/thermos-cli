66a6bb80c13fb008b5317c2b030301d9eceb0eec
mwangi-njuguna <mwanginjuguna59@gmail.com>
2018-04-24 19:11:48 +0300

    Created main method  in the cli module

--- thermos/cli.py
+++ thermos/cli.py
@@ -13,3 +13,22 @@ Help:
   For help using this tool, please open an issue on the Github repository:
   https://github.com/mwangi-njuguna/thermos-cli
 """
+
+from inspect import getmembers, isclass
+
+from docopt import docopt
+
+from . import __version__ as VERSION
+
+
+def main():
+    """Main CLI entrypoint."""
+    import skele.commands
+    options = docopt(__doc__, version=VERSION)
+
+    for (k, v) in options.items():
+        if hasattr(skele.commands, k) and v:
+            module = getattr(skele.commands, k)
+            skele.commands = getmembers(module, isclass)
+            command = [command[1] for command in skele.commands if command[0] != 'Base'][0]
+            command = command(options)
