bb10850058cca14fb98ee842a666d2768f5f6caf
mwangi-njuguna <mwanginjuguna59@gmail.com>
2018-04-24 19:00:28 +0300

    Added docstring for available commands

---
+++ thermos/cli.py
@@ -0,0 +1,15 @@
+"""
+thermos
+Usage:
+  thermos create
+  thermos -h | --help
+  thermos --version
+Options:
+  -h --help                         Show this screen for available options.
+  --version                         Show the version.
+Examples:
+  thermos hello
+Help:
+  For help using this tool, please open an issue on the Github repository:
+  https://github.com/mwangi-njuguna/thermos-cli
+"""
