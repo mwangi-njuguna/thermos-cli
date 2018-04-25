6c37ee3d54b3d7c8dff0790a6093a231700a59dd
christinekarimi <karimikim3@gmail.com>
2018-04-25 15:33:10 +0300

    Modified the setup to remove the package lib

--- setup.py
+++ setup.py
@@ -30,3 +30,41 @@ class RunTests(Command):
         errno = call(['py.test', '--cov=thermos', '--cov-report=term-missing'])
         raise SystemExit(errno)
 
+setup(
+    name = 'thermos',
+    version = __version__,
+    description = 'A command line program in Python for creating flask standard application structure, blueprints and templates.',
+    long_description = long_description,
+    url = 'https://github.com/mwangi-njuguna/thermos-cli',
+    author = 'Mwangi Njuguna',
+    author_email = 'mwanginjuguna59@gmail.com',
+    license = 'UNLICENSE',
+    classifiers = [
+        'Intended Audience :: Flask Developers',
+        'Topic :: Utilities',
+        'License :: Public Domain',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords = 'cli',
+    packages = find_packages(exclude=['docs', 'tests*']),
+    #package_dir = {'': 'lib'},
+    install_requires = ['docopt'],
+    extras_require = {
+        'test': ['coverage', 'pytest', 'pytest-cov'],
+    },
+    entry_points = {
+        'console_scripts': [
+            'thermos=thermos.cli:main',
+        ],
+    },
+    cmdclass = {'test': RunTests},
+)
